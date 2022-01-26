1faba2e0630547e210e80af6b4f8be8d0e645f48
cengizyilmaz <350462Cngz>
2022-01-26 09:58:36 +0300

    fibonacci sayilari

---
+++ src/frontEndAutomationSoner/firstHomework/Fibonacci.java
@@ -0,0 +1,27 @@
+package frontEndAutomationSoner.firstHomework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int smallerNumbers = 59;
+        System.out.println(getFibonacciList(smallerNumbers));
+    }
+
+    // "number" sayısından küçük fibonacci sayılarını verir
+    private static List<Integer> getFibonacciList(Integer number) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(0, 1, 1));
+        for (int i = 2; i < list.size(); i++) {
+            int tempInt;
+            tempInt = list.get(i) + list.get(i - 1);
+            if (number > tempInt) {
+                list.add(tempInt);
+            }
+        }
+        return list;
+    }
+
+}
